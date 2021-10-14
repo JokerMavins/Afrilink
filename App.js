bde8835fed9c63bfdf4846ca4db83e90cdacac50
JokerMavins <jbabadoudourichmir@gmail.com>
2021-10-14 12:16:53 +0100

    Add files

---
+++ App.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const App = () => {
+  return <View />;
+}
+
+export default App;
